d06da90520d557a05b245356c99f8d4f0205a997
Glauco Pereira <glaucoadm@gmail.com>
2025-01-21 13:16:42 -0300

    First Commit

---
+++ Variaveis_N1_NC4/variaveis_n1_nc4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+    
+    // Tipos primitivos:
+
+    // Tipo_variável nome_variável; forma de declaração
+
+    int idade;
+    /*
+    int Idade;
+    int IDADE;
+    int idadE;
+    => todas diferentes (case sensitive)
+    */
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3; // Float valores maiores, maior precisão do que o float
+    char letra = 'A'; // armazena apenas 1 caracter
+    char nome[20] = 'Glauco'; // É necessário definir tamanho do array 
+    int _teste = 0;
+    /*
+    int 2teste; 
+    => não pode iniciar em números
+    */
+    int t2;
+    int teste_2;
+    /*
+    int int;
+    int return; 
+    => Palavras resevadas não são permitidas
+    */
+
+    idade = 25;
+
+    quantidade = 10;
+
+
+}
