e51a8a81c5f24c716dfa114e8ece38b251eb7612
Glauco Pereira <glaucoadm@gmail.com>
2025-01-28 15:44:18 -0300

    Decremento

--- N2_NC3_operadores/n2_nc3_3_incremento_decremento.c
+++ N2_NC3_operadores/n2_nc3_3_incremento_decremento.c
@@ -11,18 +11,35 @@ int main(){
     Pós-Decremento (a--)
     */
 
-    int numero1 =1;
+    int numero1 = 1, resultado;
 
     printf("Antes incremento: %d\n", numero1);
 
     // numero1 = numero1 + 1
     // numero1 += 1
-    numero1 ++;
-    printf("Após incremento: %d\n", numero1);
+//    numero1 ++;
+//    printf("Após incremento: %d\n", numero1);
+    
+    // Pós-Incremento:
+    //  >> ressultado = numero1;
+    //  >> numero1++;
+    resultado = numero1++;
+    printf("Apos Pos-Incremento - Numero 1: %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = ++numero1;
+    printf("Apos Pre-Incremento - Numero 1: %d - Resultado: %d\n", numero1, resultado);
 
     // numero1 = numero1 - 1
     // numero1 -= 1
-    numero1 --;
-    printf("Após decremento: %d\n", numero1);
+//    numero1 --;
+//    printf("Após decremento: %d\n", numero1);
+
+    resultado = numero1--;
+    printf("Apos Pos-Decremento - Numero 1: %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = --numero1;
+    printf("Apos Pre-Decremento - Numero 1: %d - Resultado: %d\n", numero1, resultado);
+
+
 
 }
