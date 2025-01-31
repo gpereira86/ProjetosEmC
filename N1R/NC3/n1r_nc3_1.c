fb28e6881146369bbc756ec47643e7e48451c6fa
Glauco Pereira <glaucoadm@gmail.com>
2025-01-31 15:51:52 -0300

    IF-ELSE

--- N1R/NC3/n1r_nc3_1.c
+++ N1R/NC3/n1r_nc3_1.c
@@ -1,3 +1,18 @@
-//
-// Created by glauc on 31/01/2025.
-//
+#include <stdio.h>
+
+int main()
+{
+  int numero1;
+  int numero2;
+
+  numero1 = 10;
+  numero2 = 5;
+    
+  if (numero1 > numero2) {
+    printf("O número 1 é maior que o número 2\n");
+  } else {
+    printf("O número 1 é menor que o número 2\n");
+  }
+  
+  return 0;
+}
---
+++ N1R/NC3/n1r_nc3_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int nota = 50;
+
+    if (nota >= 60){
+        printf("Parabéns, você passou!");
+    } else {
+        printf("Infelizmente, você não passou!");
+    }
+
+    return 0;
+}
