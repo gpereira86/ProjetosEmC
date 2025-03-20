f0d6a457502a511fdb9e55c625bb0e473440d368
Glauco Pereira <glaucoadm@gmail.com>
2025-03-20 17:57:02 -0300

    arquivos

--- Maxiprod_C_Sharp/ControleDeGastosWeb/obj/Debug/net9.0/ControleDeGastosWeb.AssemblyInfo.cs
+++ Maxiprod_C_Sharp/ControleDeGastosWeb/obj/Debug/net9.0/ControleDeGastosWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleDeGastosWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096f08735a49128491fb206cb8d7b383490491e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786a66ccfcea61ff999b28761aa79283a5825a07")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleDeGastosWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleDeGastosWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Maxiprod_C_Sharp/MaxiprodApp/obj/Debug/net9.0/MaxiprodApp.AssemblyInfo.cs
+++ Maxiprod_C_Sharp/MaxiprodApp/obj/Debug/net9.0/MaxiprodApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaxiprodApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006f3748a11780c7c735a0c9c14784ae256b20f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786a66ccfcea61ff999b28761aa79283a5825a07")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaxiprodApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaxiprodApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
